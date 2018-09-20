9e41c8deb90e3d3d8b87666a9c19235030ab138f
Quan Nguyen <quan.nguyen@niteco.se>
2018-09-20 21:08:57 +0700

    Adding BuildScripts

---
+++ Assets/Editor/BuildScripts.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildScripts {
+	static void PerformBuild() {
+		string[] scenes = {"Assets/Scenes/SampleScene.unity"};
+		BuildPipeline.BuildPlayer(scenes, "iOSBuild", BuildTarget.iOS, BuildOptions.None);
+	}
+}
